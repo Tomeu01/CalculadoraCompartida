7594e2666387da87fe765d21fc57758bd6d3acde
Tomeu01 <tft00@iesemilidarder.com>
2025-01-13 19:55:00 +0100

    Add files via upload

---
+++ Calculadora/test/CalculadoraJUnitTest.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import java.util.Scanner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author tomeu
+ */
+public class CalculadoraJUnitTest {
+    
+    public CalculadoraJUnitTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Introduce el primer número: ");
+        double numero1 = scanner.nextDouble();
+
+        
+        System.out.print("Introduce el segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        
+        System.out.println("Elige una operación:");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicación");
+        System.out.println("4. División");
+        System.out.print("Introduce el número de la operación: ");
+
+        
+        int opcion = scanner.nextInt();
+        double resultado;
+
+        
+        switch (opcion) {
+            case 1:
+                resultado = numero1 + numero2;
+                System.out.println("El resultado de la suma es: " + resultado);
+                break;
+            case 2:
+                resultado = numero1 - numero2;
+                System.out.println("El resultado de la resta es: " + resultado);
+                break;
+            case 3:
+                resultado = numero1 * numero2;
+                System.out.println("El resultado de la multiplicación es: " + resultado);
+                break;
+            case 4:
+                if (numero2 != 0) {
+                    resultado = numero1 / numero2;
+                    System.out.println("El resultado de la división es: " + resultado);
+                } else {
+                    System.out.println("Error: No se puede dividir entre cero.");
+                }
+                break;
+            default:
+                System.out.println("Opción no válida. Por favor, elige una opción entre 1 y 4.");
+                break;
+        }
+
+    }
+}
